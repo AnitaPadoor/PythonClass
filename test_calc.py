4c589eab0733a4409e57e6782e23945e93bbb1b3
Anita Bhat <anitantry@gmail.com>
2019-06-17 14:59:32 +0200

    test_calc 17th june!

--- test_calc.py
+++ test_calc.py
@@ -1,6 +1,7 @@
 import unittest
 import calc
 class Testcalc(unittest.TestCase):
+"""" testing of addition of 2 numbers """"
     def test_add(self):
 	    result = calc.add(3,4)
 	    self.assetEquals(result,7)
