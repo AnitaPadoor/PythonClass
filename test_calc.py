5a9549358c45317c81fa1bf39548a4380595e0a6
Anita Bhat <anitantry@gmail.com>
2019-06-18 08:57:08 +0200

    aseert fixed

--- test_calc.py
+++ test_calc.py
@@ -1,7 +1,7 @@
 import unittest
 import calc
 class Testcalc(unittest.TestCase):
-"""" testing of addition of 2 numbers """"
+    """ testing of addition of 2 numbers """
     def test_add(self):
 	    result = calc.add(3,4)
-	    self.assetEquals(result,7)
+	    self.assertEquals(result,7)
