a18e7c11442da59b07ea5b5585366c09d884579a
Anita Bhat <anitantry@gmail.com>
2019-06-19 15:14:30 +0200

    test_caldć

--- test_calc.py
+++ test_calc.py
@@ -1,8 +1,12 @@
+"""Test_calc"""
 import unittest
 import calc
 class Testcalc(unittest.TestCase):
-    """ testing of addition of 2 numbers """
+    """ testing of addition of 2 numbers
+    """
     def test_add(self):
-	    result = calc.add(3, 4)
-	    self.assertEquals(result, 7)
+        """Method docstring
+        """
+        result = calc.add(3, 4)
+        self.assertEqual(result, 7)
 		
