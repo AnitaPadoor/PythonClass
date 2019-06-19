768e4ccf57d565355412862fe7d259fac866c748
Anita Bhat <anitantry@gmail.com>
2019-06-19 10:21:04 +0200

    19 june

--- calc.py
+++ calc.py
@@ -1,5 +1,6 @@
+"""Test"""
 def add(first_arg, second_arg):
-""" addition of 2 numbers """
+    """addition of 2 numbers
+    added docstring"""
     return first_arg + second_arg
-
 print add(3, 4)
